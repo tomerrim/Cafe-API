d1062f6db594e89d8a81c533edca580cd5350198
Tomer <tomerrim@gmail.com>
2023-11-04 12:05:43 +0200

    added get random cafe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 from database import db, Cafe
+import random
 
 app = Flask(__name__)
 
@@ -16,6 +17,27 @@ def get_all_cafes():
     result = db.session.execute(db.select(Cafe))
     all_cafes = result.scalars().all()
     return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+
+@app.route('/random')
+def get_random_cafe():
+    result = db.session.execute(db.select(Cafe))
+    all_cafes = result.scalars().all()
+    random_cafe = random.choice(all_cafes)
+    return jsonify(
+        cafe={
+            "id": random_cafe.id,
+            "name": random_cafe.name,
+            "map_url": random_cafe.map_url,
+            "img_url": random_cafe.img_url,
+            "location": random_cafe.location,
+            "seats": random_cafe.seats,
+            "has_toilet": random_cafe.has_toilet,
+            "has_wifi": random_cafe.has_wifi,
+            "has_sockets": random_cafe.has_sockets,
+            "can_take_calls": random_cafe.can_take_calls,
+            "coffee_price": random_cafe.coffee_price
+        }
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
